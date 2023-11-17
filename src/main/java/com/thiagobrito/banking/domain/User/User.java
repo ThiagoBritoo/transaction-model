17e9d210b6c67044060a15242a927596a3ccd921
Thiago Brito <thiagobrito74@gmail.com>
2023-11-17 09:56:43 -0400

    User entity

---
+++ src/main/java/com/thiagobrito/banking/domain/User/User.java
@@ -0,0 +1,123 @@
+package com.thiagobrito.banking.domain.User;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;        
+    private String password;
+    private BigDecimal balance;
+
+    @Column(unique = true)
+    private String document;
+    
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User(){
+    }
+
+    public User(String name, String password, BigDecimal balance, String document, String email, UserType userType) {
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
+        this.document = document;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }    
+}
---
+++ src/main/java/com/thiagobrito/banking/domain/User/UserType.java
@@ -0,0 +1,7 @@
+package com.thiagobrito.banking.domain.User;
+
+public enum UserType {
+    COMMON,
+    MERCHANT;
+
+}
